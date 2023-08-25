d6b27b226425279d009c38b2e88f3e483f69761c
Meiro17 <76156969+Meiro17@users.noreply.github.com>
2023-08-25 21:50:58 +0200

    listen port connection fix

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
-app.use(express.json());
-
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en el puerto ${PORT}`);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
