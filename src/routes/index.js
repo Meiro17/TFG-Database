4638376e1ec2eb12cd7dfcd17e490eb844f8b120
Meiro17 <76156969+Meiro17@users.noreply.github.com>
2023-08-26 00:30:25 +0200

    app localhost success

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dataController = require('../controllers/DataController');
 
 router.get('/users', dataController.getUsers);
+
 // router.post('/add-user', dataController.addUser);
 
 module.exports = router;
